a8c182bcf1f5e9130658a141666f7e766dc25c3b
Kangker <103891387+kangmin040127@users.noreply.github.com>
2024-06-20 17:22:32 +0900

    Add files via upload

---
+++ js/aeeBtn.js
@@ -0,0 +1,14 @@
+// const remainTime = document.querySelector("#remain-time");
+// function diffDay() {
+//     const startDay = new Date("2023-12-01");
+//     const nowDay = new Date();
+//     const diff = nowDay - startDay;
+//     const diffDay = Math.floor(diff / (1000*60*60*24));
+//     const diffHour = Math.floor((diff / (1000*60*60)) % 24);
+//     const diffMin = Math.floor((diff / (1000*60)) % 60);
+//     const diffSec = Math.floor(diff / 1000 % 60);
+//     remainTime.innerText = `${diffDay}일 ${diffHour}시간 ${diffMin}분 ${diffSec}초`;
+// }
+
+// diffDay()
+// setInterval(diffDay, 1000);
